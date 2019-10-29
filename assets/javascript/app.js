22bf463aeb8d68afe440195693cf616b1ef53ec8
tyronebayasen99 <tyronejbayasen@gmail.com>
2019-10-29 10:44:09 -0700

    added moment.js and converted displayClock() to moment.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,38 @@
 $(document).ready(function() {
+    //initialize global variables
+    var trainName = "";
+    var trainDestination = "";
+    var firstTrain = "";
+    var trainFrequency = "";
+    var nextTrain = "";
+    var minutesAway = "";
+    var currentTime = ""
+
     function displayClock() {
-    var currentTime = new Date();
-    var hour = currentTime.getHours();
-    var minute = currentTime.getMinutes();
-    var seconds = currentTime.getSeconds();
-    var timer = setTimeout(displayClock, 500);
+        currentTime = moment();
+        currentTime = moment(currentTime).format("hh:mm:ss A")
+        $("#displayTime").text("Current Time: " + currentTime);
+        timer =setTimeout(displayClock, 500)
+    }
 
-    minute = checkTime(minute);
-    seconds = checkTime(seconds);
+    // function displayClock() {
+    //     currentTime = new Date();
+    //     hour = currentTime.getHours();
+    //     minute = currentTime.getMinutes();
+    //     seconds = currentTime.getSeconds();
+    //     timer = setTimeout(displayClock, 500);
 
-    $("#displayTime").text("Current Time: " + hour + ":" + minute + ":" + seconds)
-    };
+    //     minute = checkTime(minute);
+    //     seconds = checkTime(seconds);
 
-    function checkTime(i) {
-        if (i < 10) {i = "0" + i};
-        return i;
-    };
+    //     $("#displayTime").text("Current Time: " + hour + ":" + minute + ":" + seconds)
+    //     return minute;
+    // };
+
+    // function checkTime(i) {
+    //     if (i < 10) {i = "0" + i};
+    //     return i;
+    // };
 
     displayClock();
 
@@ -24,18 +41,25 @@ $(document).ready(function() {
         event.preventDefault();
 
         //pull and store values from input form
-        var trainName = $("#train-name").val().trim();
-        var trainDestination = $("#destination").val().trim();
-        var firstTrainTime = $("#first-train-input").val().trim();
-        var trainFrequency = $("#frequency-input").val().trim();
+        trainName = $("#train-name").val().trim();
+        trainDestination = $("#destination").val().trim();
+        firstTrain = $("#first-train-input").val().trim();
+        trainFrequency = $("#frequency-input").val().trim();
         
-        console.log(trainName);
-        console.log(trainDestination);
-        console.log(firstTrainTime);
-        console.log(trainFrequency);
+        // console.log(trainName);
+        // console.log(trainDestination);
+        // console.log(firstTrainTime);
+        // console.log(trainFrequency);
 
-        var nextTrain;
-        var minutesAway;
+        nextTrain;
+        minutesAway;
+
+        function calcNextTrain(minute) {
+            var nextTrainTime = minute + trainFrequency;
+            console.log(nextTrainTime);
+            
+        }
+        calcNextTrain();
 
         var trainTable = $(".schedule-table");
         trainTable.append("<tr><td>" + trainName + "</td><td>" + trainDestination
